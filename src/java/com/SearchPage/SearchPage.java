9e1bab9c51d3e920ef67e72d9a3ecaf46277f8b0
vita_ <vita_@Alienware>
2016-07-07 15:00:39 +0800

    增加更多页面的managedbeans

--- src/java/com/SearchPage/SearchPage.java
+++ src/java/com/SearchPage/SearchPage.java
@@ -11,20 +11,57 @@ package com.SearchPage;
  */
 import com.HomePage.HomePage;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import sun.util.logging.PlatformLogger;
 
 @SessionScoped
-@ManagedBean(name = "home")
+@ManagedBean(name = "search")
 
 public class SearchPage implements Serializable {
 
     private String menuSearch;
     private String searchText;
+    public static boolean isFromSearchPage;
+    public static String productToRender;
 
     public SearchPage() {
 
         menuSearch = HomePage.menuSearchTem;
         searchText = HomePage.searchTextTem;
+
+//        somethings = new ArrayList<>();
+//        SomethingOfferer=SomthingOfferer.getSomethingOfferer;
     }
+//
+//     private List<Something> somethings;
+//     private SomethingOfferer SomethingOfferer; 
+
+//    public void loadSomething() {
+//
+//        Logger.info("Loading Something");
+//
+//        something.clear();
+//
+//        try {
+//
+//            somethings = SomethingOfferer.getsomething();
+//        } catch (Exception exc) {
+//
+//            logger.log(Level.SEVERE, "Error when loading something", exc);
+//
+//            System.out.println(exc);
+//        }
+//    }
+    public String getProductToRender() {
+        return productToRender;
+    }
+
+    public void setProductToRender(String productToRender) {
+        this.productToRender = productToRender;
+    }
+
 }
