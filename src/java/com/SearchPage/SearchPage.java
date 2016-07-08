c66f723799344918c60a3eec051fd6f850bc1a81
vita_ <vita_@Alienware>
2016-07-08 14:13:40 +0800

    增加更多页面的managedbeans

--- src/java/com/SearchPage/SearchPage.java
+++ src/java/com/SearchPage/SearchPage.java
@@ -10,33 +10,42 @@ package com.SearchPage;
  * @author vita_
  */
 import com.HomePage.HomePage;
+import com.LoginController.LoginController;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import sun.util.logging.PlatformLogger;
 
-@SessionScoped
+@RequestScoped
 @ManagedBean(name = "search")
 
 public class SearchPage implements Serializable {
-
+    
+    private String username;
     private String menuSearch;
     private String searchText;
     public static boolean isFromSearchPage;
     public static String productToRender;
 
     public SearchPage() {
-
+        
+        username = LoginController.usernamePrint;
         menuSearch = HomePage.menuSearchTem;
         searchText = HomePage.searchTextTem;
 
 //        somethings = new ArrayList<>();
 //        SomethingOfferer=SomthingOfferer.getSomethingOfferer;
     }
-//
+
+    public static void setIsFromSearchPage() {
+        isFromSearchPage = true;
+    }
+    
+
+
 //     private List<Something> somethings;
 //     private SomethingOfferer SomethingOfferer; 
 
@@ -56,6 +65,7 @@ public class SearchPage implements Serializable {
 //            System.out.println(exc);
 //        }
 //    }
+    
     public String getProductToRender() {
         return productToRender;
     }
