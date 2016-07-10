d546e2afd83e866028828baa110b143a9efd5c0a
vita_ <vita_@Alienware>
2016-07-10 11:10:44 +0800

    将全局变量更新至super claa 并使用protected方法调用

--- src/java/com/CertainPage/CertainPage.java
+++ src/java/com/CertainPage/CertainPage.java
@@ -5,28 +5,42 @@
  */
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
-import com.LoginController.LoginController;
-import com.SearchPage.SearchPage;
 import com.MyOrder.MyOrder;
+import com.SuperClass.SuperClass;
 import javax.faces.bean.RequestScoped;
 
 @RequestScoped
 @ManagedBean(name = "product")
 
-public class CertainPage implements Serializable {
+public class CertainPage extends SuperClass implements Serializable {
 
     private String username;                               //储存用于导航栏显示的用户名称
     private String productName;                            //储存用于展示的具体的产品名称 
 
     CertainPage() {
 
-        username = LoginController.usernamePrint;
-        if (MyOrder.productToRender != null) {
-            productName = MyOrder.productToRender;
-            MyOrder.productToRender = null;
-        } else {
-            productName = SearchPage.productToRender;
-            SearchPage.productToRender = null;
-        }
+        setUsernameFromSuperClass();
+        productName = super.getProductToRender();
     }
+
+    public void setUsernameFromSuperClass() {
+        setUsername(super.getUsernamePrint());
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
 }
